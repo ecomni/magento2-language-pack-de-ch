08e181ae4517a5397bd9292419cdc53c73e4dd4e
Fabian Schweizer <fa.schweizer@gmail.com>
2018-04-13 19:00:09 +0200

    Add first version

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Magento2 German Language Pack (de_CH)
+ *
+ * Github: https://github.com/Chuvisco88/magento2-language-pack-de-ch
+ * Packagist: https://packagist.org/packages/chuvisco88/magento2-language-pack-de-ch
+ *
+ * @author Fabian Schweizer <fa.schweizer@gmail.com>
+ * @copyright Copyright © 2018
+ * @version 1.0.0
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'chuvisco88_de_ch',
+    __DIR__
+);
